d88555b1cae8d11d61ed1b09ae61d4a0d943111d
alex6095 <l.alex6095@gmail.com>
2021-05-16 13:59:34 +0900

    Add Route
    Add Route

--- main_page/src/components/App.js
+++ main_page/src/components/App.js
@@ -1,13 +1,42 @@
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import TagSearchPage from './views/TagSearchPage/TagSearchPage';
 import TagSearchResultPage from './views/TagSearchResultPage/TagSearchResultPage'
 import ActivityInfoPage from './views/ActivityInfoPage/ActivityInfoPage'
 
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <ActivityInfoPage />
-    </div>
+    <Router>
+      <div>
+        
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <TagSearchPage />
+          </Route>
+          <Route exact path="/result">
+            <TagSearchResultPage />
+          </Route>
+          <Route exact path="/info">
+            <ActivityInfoPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
