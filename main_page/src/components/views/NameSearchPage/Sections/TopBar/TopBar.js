1367bf05eae3f8bd6ff388d817f3d99aa5bb710d
chlee973 <chlee973@kaist.ac.kr>
2021-05-22 17:52:53 +0900

    MAKE NameSearchPage

---
+++ main_page/src/components/views/NameSearchPage/Sections/TopBar/TopBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './TopBar.scss'
+
+const TopBar = ({isSignedIn, userName}) => {
+    if (isSignedIn) {
+        return (
+            <div className="TopBar">
+                <span className="userName">{userName}</span>
+                <span className="menu">Sign Out</span>
+            </div>
+        )
+    }
+    return (
+        <div className="TopBar">
+            <a className="menu" href="/login">Sign In</a>
+            <span className="menu" href="/register">Sign Up</span>
+        </div>
+    )
+
+}
+
+export default TopBar
