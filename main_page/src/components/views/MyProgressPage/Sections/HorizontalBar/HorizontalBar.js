95d0fa303df1adf0a9af6c7255df0ad0badfd7e5
OskarBronski <danke417@gmail.com>
2021-05-22 21:33:27 +0900

    Change tags of horizontal bars

--- main_page/src/components/views/MyProgressPage/Sections/HorizontalBar/HorizontalBar.js
+++ main_page/src/components/views/MyProgressPage/Sections/HorizontalBar/HorizontalBar.js
@@ -4,11 +4,24 @@ import { Nav, Navbar } from 'react-bootstrap'
 import './HorizontalBar.scss'
 
 const HorizontalBar = () => {
+
+    function clickM1() {
+        document.getElementById('MMP-percentage').scrollIntoView({behavior: "smooth"});
+    }
+
+    function clickM2() {
+        document.getElementById('MMP-selectedachiev').scrollIntoView({behavior: "smooth"});
+    }
+
+    function clickM3() {
+        document.getElementById('MMP-reviews').scrollIntoView({behavior: "smooth"});
+    }
+
     return (
         <nav id="MMP-hori-bar">
-            <a href="#MMP-percentage">Progress Percentage</a>
-            <a href="#MMP-selectedachiev">Selected Achievements</a>
-            <a href="#MMP-reviews">Reviews</a>
+            <a onClick={clickM1}>Progress Percentage</a>
+            <a onClick={clickM2}>Selected Achievements</a>
+            <a onClick={clickM3}>Reviews</a>
         </nav>
 
     )
