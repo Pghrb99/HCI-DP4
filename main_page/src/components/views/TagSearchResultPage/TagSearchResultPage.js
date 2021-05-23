4b543bcdd01e977f45f3811a2aa13c4a45a26737
chlee973 <chlee973@kaist.ac.kr>
2021-05-23 16:40:06 +0900

    EDIT ActivityCard

--- main_page/src/components/views/TagSearchResultPage/TagSearchResultPage.js
+++ main_page/src/components/views/TagSearchResultPage/TagSearchResultPage.js
@@ -11,98 +11,98 @@ const TagSearchResultPage = (props) => {
     const cards = [
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[5,7,5.6,9,10]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1.2,9,3,4.6,10]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[10,2.5,6,8,10]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[7,5,5,6,]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
     },
     {
         name: "Ice Hockey",
-        imgSrc: img1,
+        img: {alt: "Ice Hockey", src: img1},
         tags:[{name: "Soccer"},{name : "Ice Hockey"},{name: "Soccer"},{name: "Soccer"},{name: "Soccer"}],
         chartData:[1,2,3,4,5]
 
