86ed8978eb4bc0e27db3fd794bf6322f60d4e468
alex6095 <l.alex6095@gmail.com>
2021-05-26 02:22:51 +0900

    Sidemenu fix

--- main_page/src/components/views/LoginPage/LoginPage.js
+++ main_page/src/components/views/LoginPage/LoginPage.js
@@ -74,68 +74,70 @@ function LoginPage(props) {
   };
 
   return (
-    <Layout style={{backgroundColor: "rgb(247, 247, 247)"}}>
-      <Header style={{height: "80px", backgroundColor: "rgb(247, 247, 247)"}}>
-        <Sidemenu />
-      </Header>
-      
-      <Content className="container">
-        <Row>
-          <Col xs={{span: 16, offset: 4}}  sm={{span: 8, offset: 8}}>
-            <Link to={"/"} className="logo"> <img src={logo}/> </Link>
-          </Col>
-        </Row>
-        <Form
-          {...formItemLayout}
-          name="normal_login"
-          className="login-form"
-          initialValues={{ 
-            remember: true,
-            username: cookies.rememberEmail,
-           }}
-          onFinish={onFinish}
-        >
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please input your Username!' }]}
+    <>
+      <Sidemenu />
+      <Layout style={{backgroundColor: "rgb(247, 247, 247)"}}>
+        <Header style={{height: "80px", backgroundColor: "rgb(247, 247, 247)"}}>
+        </Header>
+        
+        <Content className="container">
+          <Row>
+            <Col xs={{span: 16, offset: 4}}  sm={{span: 8, offset: 8}}>
+              <Link to={"/"} className="logo"> <img src={logo}/> </Link>
+            </Col>
+          </Row>
+          <Form
+            {...formItemLayout}
+            name="normal_login"
+            className="login-form"
+            initialValues={{ 
+              remember: true,
+              username: cookies.rememberEmail,
+            }}
+            onFinish={onFinish}
           >
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: 'Please input your Username!' }]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Please input your Password!' }]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <a className="login-form-forgot" href="">
+                Forgot password
+              </a>
             </Form.Item>
 
-            <a className="login-form-forgot" href="">
-              Forgot password
-            </a>
-          </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              <Space>
+                <Button disabled={loading} type="primary" htmlType="submit" className="login-form-button">
+                  Log in
+                </Button>
+                Or <Link to={"/register"}>register now!</Link>
+              </Space>
+            </Form.Item>
+          </Form>
 
-          <Form.Item {...tailFormItemLayout}>
-            <Space>
-              <Button disabled={loading} type="primary" htmlType="submit" className="login-form-button">
-                Log in
-              </Button>
-              Or <Link to={"/register"}>register now!</Link>
-            </Space>
-          </Form.Item>
-        </Form>
-
-      </Content>
-      <Footer style={{height: "80px", backgroundColor: "rgb(247, 247, 247)"}}>
-        {error && <Alert message={error} showIcon type="error" closable onClose={onClose}/>}
-      </Footer>
-    </Layout>
+        </Content>
+        <Footer style={{height: "80px", backgroundColor: "rgb(247, 247, 247)"}}>
+          {error && <Alert message={error} showIcon type="error" closable onClose={onClose}/>}
+        </Footer>
+      </Layout>
+    </>
 
   );
 }
